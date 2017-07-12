b5f9606a2f923e4b91ae1e66ae517c3500ecc0f3
Serkan Aysan <serkanaysann@gmail.com>
2017-07-12 18:11:43 +0300

    change socket url

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://54.245.144.26/');
 
 //Query DOM
 var message = document.getElementById('message'),
