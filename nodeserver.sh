bf6c0469c715b2f1f2ae3777aa0f0a9c30c549e8
Serkan Aysan <serkanaysann@gmail.com>
2017-07-12 18:04:10 +0300

    first commit

---
+++ nodeserver.sh
@@ -0,0 +1,17 @@
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo apt-get -y install nodejs -y
+sudo apt-get -y install build-essential
+sh nodesource_setup.sh
+apt-get -y install nodejs
+sudo npm install -g pm2
+cd /home/ubuntu/git/websocket
+pm2 start index.js
+pm2 startup systemd
+sudo su -c "env PATH=$PATH:/usr/bin pm2 startup systemd -u ubuntu --hp /home/ubuntu"
+systemctl status pm2
+pm2 list
+#install nginx
+sudo apt-get -y install nginx
+yes|cp -rf ./nginx/sites-available/default /etc/nginx/sites-available/default
+nginx -t
+systemctl restart nginx
